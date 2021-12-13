59b0b1e69f054f0ec80264d07f1956c261be6379
Iuliia Stetskaia <iuliia_stetskaia@epam.com>
2021-12-13 21:44:09 +0300

    Add argument "format of output"

--- gendiff/cli.py
+++ gendiff/cli.py
@@ -6,6 +6,7 @@ def get_parser():
 
     parser.add_argument('first_file')
     parser.add_argument('second_file')
+    parser.add_argument('-f', '--format', help='set format of output')
 
     return parser
 
